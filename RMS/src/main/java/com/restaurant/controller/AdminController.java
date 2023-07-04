1536ef8d568c212d7575504f2288015e13e894c2
Nagachetan <nagachetan14@gmail.com>
2023-07-04 02:29:58 -0500

    server added

--- RMS/src/main/java/com/restaurant/controller/AdminController.java
+++ RMS/src/main/java/com/restaurant/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.restaurant.model.Admin;
 import com.restaurant.model.Customer;
@@ -17,9 +18,10 @@ import com.restaurant.service.AdminService;
 import com.restaurant.service.RegistrationService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping(value = "rms")
+@RequestMapping(value = "admin")
 public class AdminController {
 	
 	@Autowired
@@ -29,30 +31,37 @@ public class AdminController {
 	private AdminService adminService;
 	
 	
-	@RequestMapping()
-	public String home() {
-		return "home";
-	}
-	
 	@PostMapping("register")
 	public boolean saveCustomer(@RequestBody Customer customer) {
 		return registrationService.saveCustomer(customer);
 	}
 	
-	@GetMapping("adminLogin")
+	@GetMapping("login")
 	public String adminLogin() {
 		return "adminLogin";
 	}
 	
-	@PostMapping("adminLogin")
-	public String adminLogin1(Admin admin, Model model, HttpServletRequest req) {
+	@PostMapping("login")
+	public String adminLogin1(Admin admin, Model model, HttpServletRequest req,RedirectAttributes redirectAttributes) {
 		Optional<Admin> obj = adminService.findByEmailAndPassword(admin.getEmail(), admin.getPassword());
 		if (obj.isPresent()) {
-			return "adminHome";
+			return "redirect:/home";
 		} else {
-			model.addAttribute("msg", "Invalid Login Credentials");
-			return "adminLogin";
+			redirectAttributes.addAttribute("msg", "Invalid Login Credentials");
+			return "redirect:/login";
 		}
+	}
+	
+	@RequestMapping("home")
+	public String home(HttpSession session) {
+		session.setAttribute("role", "Admin");
+		return "adminHome";
+	}
+	
+	@GetMapping("logout")
+	public String logout(Model model, HttpServletRequest req) {
+		req.getSession().invalidate();
+		return "redirect:/home";
 	}
 
 }
